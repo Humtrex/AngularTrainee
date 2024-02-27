a8cd5f76ae8ec6eb8d1d2a28695821c33ca2165b
Humtrex <humberto_hdezt@hotmail.com>
2024-02-27 11:28:05 -0600

    R

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PersonaComponent } from './persona/persona.component';
+import { PersonasComponent } from './personas/personas.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, PersonaComponent, PersonasComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  titulo = 'Mundo!!';
+}
